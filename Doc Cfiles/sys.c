ba705d0ef2cbc109cd2e5d874021fc3f2900ae5a
hemasri159 <hemasrimail170@gmail.com>
2026-09-09 11:16:47 +0530

    Add files via upload

---
+++ Doc Cfiles/sys.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+
+void handler(int sig){
+printf("\nSIGNT recieved!\n");
+printf("Signal number = %d\n",sig);
+}
+int main(){
+signal(SIGINT, handler);
+while(1){
+printf("Program is running...\n");
+sleep(2);
+}
+return 0;
+}
